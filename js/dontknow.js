becb24ec916fe50774a4af7f05fe981b9ec58426
Awea <aweaoftheworld@gmail.com>
2014-01-07 09:04:48 +0100

    Site updated at 2014-01-07 08:04:48 UTC

---
+++ js/dontknow.js
@@ -0,0 +1,19 @@
+$(window).load(function () { 
+  $('#mason').masonry(); 
+}); 
+
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() != 0) {
+      $('#toTop').fadeIn();
+    } else {
+      $('#toTop').fadeOut();
+    }
+  });
+  $('#toTop').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    },
+    800);
+  });
+});
